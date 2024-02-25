fca07d517ae129c45a007d7fc470e244781de012
dev <ethtaylan@gmail.com>
2024-02-25 04:37:42 +0100

    feat: put and delete endpoints

--- people.py
+++ people.py
@@ -2,8 +2,12 @@
 
 from datetime import datetime
 
+from flask import abort, make_response
+
+
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
 
 PEOPLE = {
     "Fairy": {
@@ -20,11 +24,20 @@ PEOPLE = {
         "fname": "Easter",
         "lname": "Bunny",
         "timestamp": get_timestamp(),
-    }
+    },
 }
+
 
 def read_all():
     return list(PEOPLE.values())
+
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(404, f"Person with last name {lname} not found")
+
 
 def create(person):
     lname = person.get("lname")
@@ -42,3 +55,20 @@ def create(person):
             406,
             f"Person with last name {lname} already exists",
         )
+
+
+def update_one(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(404, f"Person with last name {lname} not found")
+
+
+def delete_one(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(f"{lname} successfully deleted", 200)
+    else:
+        abort(404, f"Person with last name {lname} not found")
